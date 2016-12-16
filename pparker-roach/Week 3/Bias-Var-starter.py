dfc8d0f52b9174f37cd9118927038abef3b7b252
Pat Parker-Roach <Pat@Parker-Roach.com>
2016-12-16 07:18:32 -0500

    Week 3 Friday morning push

--- pparker-roach/Week 3/Bias-Var-starter.py
+++ pparker-roach/Week 3/Bias-Var-starter.py
@@ -9,10 +9,10 @@
 # 
 # to some cubic polynomial data with noise.
 
-# In[1]:
+# In[5]:
 
 get_ipython().magic('matplotlib inline')
-import randomyh
+import random
 
 from matplotlib import pyplot as plt
 import numpy as np
@@ -24,7 +24,7 @@ from sklearn import datasets, linear_model, cross_validation
 # ## Some helper functions
 # The following functions will generate data and polynomial values. Not all of them are used/useful..
 
-# In[20]:
+# In[6]:
 
 def polynomials(X, degree=1):
     array = [X]
@@ -55,7 +55,7 @@ mu, sigma = 0, 0.1 # mean and standard deviation of noise
 
 # test what the functions looks do: 
 
-# In[26]:
+# In[7]:
 
 X = np.array(range(10))
 print(polynomials(X, 3)) # each row is x raised to different powers
@@ -79,7 +79,7 @@ generate_data(lambda x: x*x,0,2,10) # return x values and x^2 from 0->2 with 10 
 
 # ### Let's generate the data that will be used in the fit  and plot it: 
 
-# In[30]:
+# In[8]:
 
 # Generate some data for the formula below
 f = lambda x: 4 + 5 * x - 3 * x*x + 0.1 * x * x * x + np.random.normal(mu, sigma)
@@ -87,10 +87,11 @@ data = generate_data(f,0,2,100)
 plt.plot(pd.DataFrame(data)[0],pd.DataFrame(data)[1],"ro") # can you think of a cleaner way of ploting this? :) 
 
 
-# In[52]:
+# In[16]:
 
 def rmse(yh,h):
-    return ???
+    l=yh.size 
+    return np.sqrt(np.sum((yh-h)*(yh-h))/l)
 
 def score_rms(model,x,y):
     # how does this related to cross_val_score ? 
@@ -101,8 +102,8 @@ def estimate_error_using_cv(data,model_n):
     domain = np.array([x[0] for x in data])
     Y = np.array([x[1] for x in data])
     X = monomials(domain, model_n) # since our model is w0+w1*x^n
-    kfold =????# 8 way cross-validation, Can you guess why we are using KFold? 
-    cv_errors=cross_validation.cross_val_score(????, X, Y, scoring=score_rms, cv=kfold)
+    kfold = 8 # 8 way cross-validation, Can you uess why we are using KFold? 
+    cv_errors=cross_validation.cross_val_score(linear_model.LinearRegression(), X, Y, scoring=score_rms, cv=kfold)
     #print(cv_errors)
     return np.mean(cv_errors) #because of the way mean_squared_error is defined in scikit-learn 
     
@@ -120,17 +121,17 @@ def plot_data_and_model(data,model_n):
 # 
 # Things to medidate on:
 #     1. How can you *eyeball* the results from `estimate_error_using_cv` ?
-#     2. by walking thoughtthe models of different n's can you see which model is likely to be the best? 
+#     2. by walking though the models of different n's can you see which model is likely to be the best? 
 
-# In[53]:
+# In[28]:
 
-plot_data_and_model(data,model_n=1)
-estimate_error_using_cv(data_cv,model_n=1)
+plot_data_and_model(data,model_n=8)
+estimate_error_using_cv(data,model_n=1)
 
 
-# In[54]:
+# In[30]:
 
-cv_res=pd.DataFrame([[deg, estimate_error_using_cv(data_cv,model_n=deg)] for deg in range(15) ])
+cv_res=pd.DataFrame([[deg, estimate_error_using_cv(data,model_n=deg)] for deg in range(15) ])
 plt.plot(cv_res[0],cv_res[1],'r') # the CV curve: 
 
 
