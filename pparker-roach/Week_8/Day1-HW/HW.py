a7ce1d71f542073732451385dd75306ff3bdd6cc
Pat Parker-Roach <Pat@Parker-Roach.com>
2017-01-24 09:01:26 -0500

    homework

--- pparker-roach/Week_8/Day1-HW/HW.py
+++ pparker-roach/Week_8/Day1-HW/HW.py
@@ -16,13 +16,15 @@ if __name__ == "__main__":
     for line in sys.stdin:                             # get a line of input from input strea,
         split_line = line.split(" ")                   # break the line of input into segments at spaces
         for word in split_line:                        # for each word
-            
+            #strip punctuation
             translator = str.maketrans('', '', string.punctuation)
             stripped_word = (word.translate(translator))
+            #update dictionary with stripped_word
             if stripped_word in word_count_dictionary.keys():
                 word_count_dictionary[stripped_word] += 1
             else:
                 word_count_dictionary[stripped_word] = 1
+
 for key, item in word_count_dictionary.items():
     word_count = key + ' = ' + str(item) + '\n'
     sys.stdout.write(word_count)
