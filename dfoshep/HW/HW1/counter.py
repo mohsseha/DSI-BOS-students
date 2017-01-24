5a7d03956c7b501bb5df7e3b110be449a58f3be4
Steven Shepherd <stevenshepherd@Stevens-MacBook-Pro.local>
2017-01-24 08:59:08 -0500

    HW1

---
+++ dfoshep/HW/HW1/counter.py
@@ -0,0 +1,21 @@
+import sys
+import string
+from collections import defaultdict
+
+
+wordcounts = defaultdict(int)
+
+for line in sys.stdin:
+
+    #line = line.encode('ascii')
+
+    line = line.translate(None, string.punctuation)
+    line = line.strip()
+    words = [word.lower() for word in line.split()]
+
+    for word in words:
+        wordcounts[word] += 1
+
+
+for w, n in sorted(wordcounts.items()):
+    print '%s\t%s' %(w,n)
